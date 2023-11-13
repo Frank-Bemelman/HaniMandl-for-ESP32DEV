66613cf262ac68599ba0aa4d7ccd3ab6421d17a7
Frank Bemelman <58854621+Frank-Bemelman@users.noreply.github.com>
2023-11-13 20:27:39 +0100

    Add files via upload

--- include/hani.h
+++ include/hani.h
@@ -1,5 +1,34 @@
 // useful defines for all source files
 #define FIRMWARE_VERSION "HANIMANDL V4.0"
+
+// rotary knob has different targets
+#define SW_WINKEL    0
+#define SW_KORREKTUR 1
+#define SW_MENU      2
+
+// operation mode as set by 3 way switch
+#define MODE_SETUP       0
+#define MODE_AUTOMATIK   1
+#define MODE_HANDBETRIEB 2
+
+
+
+// Buzzer Sounds
+#define BUZZER_SHORT   1
+#define BUZZER_LONG    2
+#define BUZZER_SUCCESS 3
+#define BUZZER_ERROR   4
+
+//#define SERVO_REVERSED       // Servo invertieren, falls der Quetschhahn von links geöffnet wird. Mindestens ein Exemplar bekannt
+#ifdef SERVO_REVERSED
+  #define SERVO_WRITE(n)     servo.write(180-n)
+#else
+  #define SERVO_WRITE(n)     servo.write(n)
+#endif
+
+
+
+
 #define HANI_LOGO 0
 #define HANI_SETUP 1
 #define HANI_AUTO 2
@@ -21,10 +50,24 @@ struct TFTline
   int  backgroundcolor;
   int  canvascolor;
   bool blink;
+  bool blinkold;
   int  lastpixelwidth;
   bool rounded; // background box rounded or not
 };
 
+struct JarName
+{ char name[32];
+  char shortname[4];
+};
+
+// Füllmengen für 5 verschiedene Gläser
+struct JarParameter { 
+  int Gewicht;
+  int GlasTyp;
+  int Tara;
+  int TripCount;
+  int Count;
+};
 
 #define SWITCH_SETUP 4 
 #define SWITCH_AUTO 15
